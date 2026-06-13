53eceb5e6cf0e50e83e8f2804f3a078a735a9eaa
nanezzio <kubik.moyo06@gmail.com>
2026-06-13 04:03:46 +0300

    add examples

--- main.cpp
+++ main.cpp
@@ -1,12 +1,26 @@
 ﻿#include <iostream>
+#include <exception>
+
 #include "IdentifierGenerator.h"
 
 int main()
 {
-    std::cout << IdentifierGenerator::GenerateNextIdentifier("A1") << std::endl;
+    try
+    {
+        std::cout << "A1 -> " << IdentifierGenerator::GenerateNextIdentifier("A1") << std::endl;
 
+        std::cout << "A9 -> " << IdentifierGenerator::GenerateNextIdentifier("A9") << std::endl;
 
+        std::cout << "C9 -> " << IdentifierGenerator::GenerateNextIdentifier("C9") << std::endl;
 
+        std::cout << "Z9 -> " << IdentifierGenerator::GenerateNextIdentifier("Z9") << std::endl;
 
-	return EXIT_SUCCESS;
+        std::cout << "A1-Z9 -> " << IdentifierGenerator::GenerateNextIdentifier("A1-Z9") << std::endl;
+    }
+    catch (const std::exception& ex)
+    {
+        std::cerr << "Error: " << ex.what() << std::endl;
+    }
+
+    return 0;
 }
